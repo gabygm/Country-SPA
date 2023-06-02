56bb49d3acdf4e72083b40c6daf56b3d0dfc1db6
Gaby <paogaby0803@gmail.com>
2023-06-01 22:15:24 -0500

    feat: adds components about-page

---
+++ src/app/shared/pages/about-page/about-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'share-about-page',
+  templateUrl: './about-page.component.html',
+  styleUrls: []
+})
+export class AboutPageComponent {
+
+}
