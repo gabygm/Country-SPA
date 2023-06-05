675ba04d6ee7a214cddca479a168ae069cfb0965
Gaby <paogaby0803@gmail.com>
2023-06-04 20:58:29 -0500

    feat: add routes region, country

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomePageComponent } from './shared/pages/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { CountriesModule } from './countries/countries.module';
 
 
 const routes: Routes = [
@@ -21,9 +22,13 @@ const routes: Routes = [
 
     },
     {
+        path: 'countries',
+        loadChildren: ()=> import('./countries/countries.module').then(m  => m.CountriesModule)
+    },
+    {
         path: '**',
         redirectTo:'home'
-    }
+    },
 
 ]
 
